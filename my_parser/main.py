6be7fa57086486b1fbb5f8992bbba1ea2365361d
johnsorry13 <john.sorry13@gmail.com>
2026-01-20 20:55:10 +0300

    Частично добавил логирование и обработку ошибок

--- my_parser/main.py
+++ my_parser/main.py
@@ -1,9 +1,26 @@
 import time
+import colorlog
 import yaml
-
 from parsers.proxy_manager import ProxyManager
 from parsers.universal_parser import UniversalParser
 from pathlib import Path
+import logging
+
+handler = colorlog.StreamHandler()
+handler.setFormatter(colorlog.ColoredFormatter(
+    "%(log_color)s%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    log_colors={
+        'DEBUG': 'cyan',
+        'INFO': 'green',
+        'WARNING': 'yellow',
+        'ERROR': 'red',
+        'CRITICAL': 'red,bg_white',
+    }
+))
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
 
 proxy_manager = ProxyManager()
 full_config_path = (Path(__file__).parent
--- my_parser/parsers/universal_parser.py
+++ my_parser/parsers/universal_parser.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from datetime import date
 import pandas as pd
 import requests
@@ -9,38 +10,64 @@ from .base_parser import BaseParser
 
 
 class UniversalParser(BaseParser):
+    logger = logging.getLogger(__name__)
     def __init__(self, store_config, proxy_manager):
         self._store_config = store_config
         self._proxy_manager = proxy_manager
+        self._urls = self.load_urls()
+
+    def load_urls(self):
+        self.logger.info("Загружаю список URL для парсинга")
+        try:
+            urls_path = self._store_config['urls_path']
+            all_urls = pd.read_excel(urls_path)
+            urls = all_urls[all_urls['shop'] == self._store_config['shop']]['URLs'].tolist()
+            self.logger.info(f"Успешно загружено {len(urls)} ссылок")
+            return urls
+        except Exception as exec:
+            self.logger.error(f"Ошибка загрузки списка URL {exec}")
+            raise
+
+
     def fetch(self, url: str):
-        proxy = self._proxy_manager.get_proxy()
+        self.logger.info(f"Загружаю прокси для {url}")
+        try:
+            proxy = self._proxy_manager.get_proxy()
+            self.logger.info(f"Прокси {proxy['host']} успешно загружен для {url}")
+        except Exception as exec:
+            self.logger.error(f"Ошибка загрузки прокси для {url}")
+            raise
         # proxies = {'http': f'http://{proxy["login"]}:{proxy["password"]}@{proxy["host"]}:{proxy["port"]}'}
 
         proxies = {
             'http': f'http://{proxy["login"]}:{proxy["password"]}@{proxy["host"]}:{proxy["port"]}',
             'https': f'http://{proxy["login"]}:{proxy["password"]}@{proxy["host"]}:{proxy["port"]}'
         }
+        self.logger.info(f"Загрузка страницы: {url}, прокси: {proxy['host']}")
+        try:
+            html = requests.get(url, proxies=proxies, timeout=10)
+            html.raise_for_status()
+            self.logger.info(f"Страница: {url} успешно загружена, прокси: {proxy['host']}")
+        except Exception as exec:
+            self.logger.error(f"Ошибка загрузки страницы: {url}, прокси: {proxy['host']}")
+            raise
+        return html, proxy
 
-        return requests.get(url, proxies=proxies, timeout=10), proxy
-
-    def parse(self, html, proxy):
+    def parse(self, html, proxy, url):
         page = BeautifulSoup(html.text, 'html.parser')
         name = page.select_one(self._store_config['name']).text
         price = page.select_one(self._store_config['price']).text
         timestamp = date.today()
-        item = {'название': name, 'цена': price, 'date': timestamp, 'proxy': proxy}
+        item = {'название': name, 'цена': price, 'date': timestamp, 'proxy': proxy['host'], 'url': url}
         return item
 
     def _parse_and_fetch(self, url):
         html, proxy = self.fetch(url)
-        return self.parse(html, proxy)
+        return self.parse(html, proxy, url)
 
     def streaming_result(self):
-        urls_path = self._store_config['urls_path']
-        all_urls = pd.read_excel(urls_path)
-        urls = all_urls[all_urls['shop'] == self._store_config['shop']]['URLs'].tolist()
         with ThreadPoolExecutor (max_workers=self._store_config['max_workers']) as executor:
-            results = {executor.submit(self._parse_and_fetch, url): url for url in urls}
+            results = {executor.submit(self._parse_and_fetch, url): url for url in self._urls[:10]}
             for future in as_completed(results):
                 url = results[future]
                 try:
@@ -58,13 +85,13 @@ class UniversalParser(BaseParser):
             with open(f'{self._store_config["shop"]}.csv', 'a', encoding='utf-8-sig', newline='') as f:
                 writer = csv.writer(f, delimiter=';')
                 if not file_exists:
-                    writer.writerow(['Название', 'Цена', 'Дата', 'Прокси'])
+                    writer.writerow(['Название', 'Цена', 'Дата', 'Прокси', 'Ошибка', 'URL'])
                 for res in self.streaming_result():
                     if "ошибка" in res:
-                        writer.writerow([f"Ошибка:{res['ошибка']}", f"URL: {res['url']}"])
+                        writer.writerow(["", "", "", "", f"{res['ошибка']}", f"{res['url']}"])
 
                     else:
-                        writer.writerow([res['название'], res['цена'], res['date']])
+                        writer.writerow([res['название'], res['цена'], res['date'], res['proxy'], "", res['url']])
         except (OSError, IOError) as e:
             print (f'Ошибка записи файла {e}')
 
